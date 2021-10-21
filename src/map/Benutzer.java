9dccefffbdfabec32211d8372b571a02ec301078
Freddies Pizza <gabrielserengheu@gmail.com>
2021-10-21 14:58:59 +0300

    Alles ausser Kommentare gemacht, mit einige system out's um zu testen

--- src/map/Benutzer.java
+++ src/map/Benutzer.java
@@ -7,6 +7,36 @@ public class Benutzer {
     private String nachName;
     private List<Sport> sport;
 
+    public void setVorName(String vorName) {
+        this.vorName = vorName;
+    }
+
+    public void setNachName(String nachName) {
+        this.nachName = nachName;
+    }
+
+    public void setSport(List<Sport> sport) {
+        this.sport = sport;
+    }
+
+    public String getVorName() {
+        return vorName;
+    }
+
+    public String getNachName() {
+        return nachName;
+    }
+
+    public List<Sport> getSport() {
+        return sport;
+    }
+
+    public Benutzer(String vorName, String nachName, List<Sport> sport) {
+        this.vorName = vorName;
+        this.nachName = nachName;
+        this.sport = sport;
+    }
+
     public double kalkuliereZeite(){
         double zeit = 0;
         for(Sport i : sport){
--- src/map/Main.java
+++ src/map/Main.java
@@ -1,8 +1,24 @@
 package map;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Sport baschet = new Basketball();
+        Sport fotbal = new Fussball();
+        Sport maraton = new Hindernislauf();
+        List<Sport> list = new ArrayList<>();
+        list.add(baschet); list.add(fotbal); list.add(maraton);
+        Benutzer user1 = new Benutzer("Andrei","Balu",list);
+
+        Sport jump = new Hochsprung();
+        System.out.println(user1.kalkuliereZeite());
+        System.out.println(user1.kalkuliereZeite(jump));
+        System.out.println(user1.kalkuliereZeite(fotbal));
+        System.out.println(user1.kalkuliereDurschnittszeit());
+
     }
 }
